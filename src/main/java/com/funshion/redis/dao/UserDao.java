015024e1ac8e7d187bb11aad592f5af7ff9d592f
zhangxu <zhangxu@FX-JSJ801.funshion.com>
2015-12-30 17:22:09 +0800

    EDFGWE

---
+++ src/main/java/com/funshion/redis/dao/UserDao.java
@@ -0,0 +1,48 @@
+package com.funshion.redis.dao;
+
+import java.util.List;
+
+import com.funshion.redis.entity.User;
+   
+public interface UserDao {  
+      
+    /** 
+     * 新增 
+     * @param user 
+     * @return 
+     */  
+    boolean add(User user);  
+      
+    /** 
+     * 批量新增 使用pipeline方式 
+     * @param list 
+     * @return 
+     */  
+    boolean add(List<User> list);  
+      
+    /** 
+     * 删除 
+     * @param key 
+     */  
+    void delete(String key);  
+      
+    /** 
+     * 删除多个 
+     * @param keys 
+     */  
+    void delete(List<String> keys);  
+      
+    /** 
+     * 修改 
+     * @param user 
+     * @return  
+     */  
+    boolean update(User user);  
+  
+    /** 
+     * 通过key获取 
+     * @param keyId 
+     * @return  
+     */  
+    User get(String keyId);  
+}  
